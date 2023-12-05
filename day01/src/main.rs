7012892e25d870036b894076e3d560d77b0264c7
Malcolm Lashley <mlashley@gmail.com>
2023-12-05 16:47:49 +0000

    Day1 - Part2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -15,10 +15,10 @@ fn test() {
     );
     debug_assert!(
         part2(
-            std::fs::read_to_string("input_sample.txt")
+            std::fs::read_to_string("input_sample2.txt")
                 .unwrap()
                 .as_str()
-        ) == 888
+        ) == 281
     );
 }
 
@@ -34,7 +34,34 @@ fn part1(data: &str) -> u32 {
     total
 }
 fn part2(data: &str) -> u32 {
-    888
+    let mut total = 0;
+    let patterns = &[
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+    let replace_with = &[
+        "o1e", "t2o", "t3e", "f4r", "f5e", "s6x", "s7n", "e8t", "n9e",
+    ];
+
+    data.trim().split('\n').for_each(|g| {
+        let mut f = String::from(g);
+
+        patterns.iter().zip(replace_with.iter()).for_each(|(a, b)| {
+            f = f.replace(a, b);
+        });
+
+        let first: Option<u32> = f.chars().find_map(|c| c.to_digit(10));
+        let last: Option<u32> = f.chars().rev().find_map(|c| c.to_digit(10));
+        // print!(
+        //     "{}, {} Ans: {}  from: {}  \n",
+        //     first.unwrap(),
+        //     last.unwrap(),
+        //     first.unwrap() * 10 + last.unwrap(),
+        //     f
+        // );
+        total += first.unwrap() * 10 + last.unwrap();
+    });
+
+    total
 }
 
 fn main() {
@@ -45,6 +72,6 @@ fn main() {
     assert!(p1 == 54953);
     let p2 = part2(std::fs::read_to_string("input.txt").unwrap().as_str());
     println!("Part2: {}", p2);
-    assert!(p2 == 888);
+    assert!(p2 == 53868);
     println!("Completed in {} us", now.elapsed().as_micros());
 }
