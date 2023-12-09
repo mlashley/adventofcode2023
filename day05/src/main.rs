d0f2572a114fda9e33be63658df95c7f6f6cecf2
Malcolm Lashley <mlashley@gmail.com>
2023-12-09 15:31:19 +0000

    Day 5 - Lashley the lazy brute-forcer :)
    Fairly sure I could have built the map in reverse and made this a simple
    lookup... but I'm still playing catchup - not proud :)
    
    [2023-12-09T15:23:51Z INFO  day05] Part1: 1181555926
    [2023-12-09T15:23:58Z INFO  day05] MAP H: Ok(MapHeader { a_name: "seed", b_name: "soil" })
    [2023-12-09T15:25:06Z INFO  day05] MAP H: Ok(MapHeader { a_name: "soil", b_name: "fertilizer" })
    [2023-12-09T15:26:32Z INFO  day05] MAP H: Ok(MapHeader { a_name: "fertilizer", b_name: "water" })
    [2023-12-09T15:27:03Z INFO  day05] MAP H: Ok(MapHeader { a_name: "water", b_name: "light" })
    [2023-12-09T15:28:16Z INFO  day05] MAP H: Ok(MapHeader { a_name: "light", b_name: "temperature" })
    [2023-12-09T15:28:50Z INFO  day05] MAP H: Ok(MapHeader { a_name: "temperature", b_name: "humidity" })
    [2023-12-09T15:29:29Z INFO  day05] MAP H: Ok(MapHeader { a_name: "humidity", b_name: "location" })
    [2023-12-09T15:30:21Z INFO  day05] Part2: 37806486

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,8 +1,8 @@
-use std::collections::VecDeque;
+use log::{debug, error, info, log_enabled, warn, Level};
 use parse_display::{Display, FromStr};
-use std::time::Instant;
-use log::{debug, error, log_enabled, info, warn, Level};
+use std::collections::VecDeque;
 use std::env;
+use std::time::Instant;
 
 fn test() {
     debug_assert!(
@@ -17,7 +17,7 @@ fn test() {
             std::fs::read_to_string("input_sample.txt")
                 .unwrap()
                 .as_str()
-        ) == 888
+        ) == 46
     );
 }
 
@@ -76,17 +76,17 @@ fn part1(data: &str) -> u64 {
         for (i, seed) in seeds.clone().into_iter().enumerate() {
             let mut mapped = false;
             for r in &ranges {
-                debug!("R: {:?}",r);
+                debug!("R: {:?}", r);
 
                 if seed >= r.src_start && seed < (r.src_start + r.length) {
                     let mapped_val = r.dst_start + (seed - r.src_start);
-                    debug!("Mapping {} => {}",seed,mapped_val);
+                    debug!("Mapping {} => {}", seed, mapped_val);
                     mapped = true;
                     seeds[i] = mapped_val;
                 }
             }
             if !mapped {
-                debug!("Mapping {} => {}",seed,seed);
+                debug!("Mapping {} => {}", seed, seed);
             }
         }
     }
@@ -94,8 +94,65 @@ fn part1(data: &str) -> u64 {
     seeds.sort();
     *seeds.first().unwrap()
 }
-fn part2(data: &str) -> usize {
-    888
+fn part2(data: &str) -> u64 {
+    let mut chunks: VecDeque<_> = data.split("\n\n").filter(|y| !y.is_empty()).collect();
+
+    let seed_obj = chunks
+        .pop_front()
+        .expect("WTF seeds?")
+        .parse::<Seeds>()
+        .unwrap();
+
+    let mut seedy: Vec<_> = seed_obj
+        .seeds
+        .split(' ')
+        .map(|s| s.parse::<u64>().unwrap())
+        .collect();
+    debug!("SEEDS: {:?}", seedy);
+
+    let mut seeds: Vec<u64> = Vec::new();
+    for pair in seedy.chunks(2) {
+        let mut a: Vec<u64> = (pair[0]..pair[0] + pair[1]).collect();
+        seeds.append(&mut a);
+
+        debug!("{:?}", pair);
+    }
+
+    while chunks.len() > 0 {
+        let mut lines: VecDeque<_> = chunks
+            .pop_front()
+            .expect("Bad Chunk?")
+            .split('\n')
+            .filter(|y| !y.is_empty())
+            .collect();
+
+        let map_hdr = lines.pop_front().expect("WTF maps?").parse::<MapHeader>();
+        info!("MAP H: {:?}", map_hdr);
+        let ranges: Vec<Range> = lines
+            .into_iter()
+            .map(|x| x.parse::<Range>().unwrap())
+            .collect();
+
+        for (i, seed) in seeds.clone().into_iter().enumerate() {
+            let mut mapped = false;
+            for r in &ranges {
+                debug!("R: {:?}", r);
+
+                if seed >= r.src_start && seed < (r.src_start + r.length) {
+                    let mapped_val = r.dst_start + (seed - r.src_start);
+                    debug!("Mapping {} => {}", seed, mapped_val);
+                    mapped = true;
+                    seeds[i] = mapped_val;
+                }
+            }
+            if !mapped {
+                debug!("Mapping {} => {}", seed, seed);
+            }
+        }
+    }
+
+    seeds.sort();
+    *seeds.first().unwrap()
 }
 
 fn main() {
