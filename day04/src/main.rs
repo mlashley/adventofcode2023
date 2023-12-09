d7d864595d2111df0fdc57f361675a76e79185a1
Malcolm Lashley <mlashley@gmail.com>
2023-12-09 13:26:20 +0000

    Day 4 - Part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -1,6 +1,3 @@
-// use itertools::Itertools;
-//use std::collections::{HashSet, VecDeque};
-// use std::hash::Hash;
 use parse_display::{Display, FromStr};
 use std::time::Instant;
 
@@ -27,11 +24,11 @@ fn test() {
             std::fs::read_to_string("input_sample.txt")
                 .unwrap()
                 .as_str()
-        ) == 888
+        ) == 30
     );
 }
 
-fn score(g: &Game) -> u32 {
+fn score(g: &Game, is_part2: bool) -> u32 {
     // println!("Game {} Scoring winners {} for {}",g.id,g.winners,g.mine );
 
     let my_numbers: Vec<u32> = g
@@ -57,6 +54,9 @@ fn score(g: &Game) -> u32 {
     if score == 0 {
         return 0;
     }
+    if is_part2 {
+        return score;
+    }
     return 2_u32.pow(score - 1);
 }
 
@@ -64,11 +64,27 @@ fn part1(data: &str) -> u32 {
     data.split('\n')
         .filter(|y| !y.is_empty())
         .map(|x| x.parse::<Game>().unwrap())
-        .map(|x| score(&x))
+        .map(|x| score(&x, false))
         .sum()
 }
 fn part2(data: &str) -> u32 {
-    888
+    let len = data.split('\n').filter(|y| !y.is_empty()).count();
+    let mut cards = vec![1; len];
+
+    data.split('\n')
+        .filter(|y| !y.is_empty())
+        .map(|x| x.parse::<Game>().unwrap())
+        .enumerate()
+        .map(|(i, x)| {
+            let s = usize::try_from(score(&x, true)).unwrap();
+            // println!("{} scored {}",i,s);
+            for id in i + 1..i + s + 1 {
+                cards[id] += cards[i];
+            }
+        })
+        .for_each(drop);
+    // println!("{:?}", cards);
+    cards.into_iter().sum()
 }
 
 fn main() {
@@ -79,6 +95,6 @@ fn main() {
     assert!(p1 == 20407);
     let p2 = part2(std::fs::read_to_string("input.txt").unwrap().as_str());
     println!("Part2: {}", p2);
-    assert!(p2 == 888);
+    assert!(p2 == 23806951);
     println!("Completed in {} us", now.elapsed().as_micros());
 }
