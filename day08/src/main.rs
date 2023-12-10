e342ac1c7efba08b7d5d75f6351aac4d77490d3d
Malcolm Lashley <mlashley@gmail.com>
2023-12-10 17:00:41 +0000

    Day 8.2

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,11 +1,12 @@
 // use itertools::Itertools;
 //use std::collections::{HashSet, VecDeque};
 // use std::hash::Hash;
-use log::{debug, error, info, log_enabled, warn, Level};
+use log::{debug, error, info};
 use parse_display::{Display, FromStr};
 use std::env;
 use std::time::Instant;
 use std::collections::HashMap;
+use num::Integer;
 
 fn test() {
     debug_assert!(
@@ -24,10 +25,10 @@ fn test() {
     );
     debug_assert!(
         part2(
-            std::fs::read_to_string("input_sample.txt")
+            std::fs::read_to_string("input_sample3.txt")
                 .unwrap()
                 .as_str()
-        ) == 888
+        ) == 6
     );
 }
 
@@ -67,8 +68,75 @@ fn part1(data: &str) -> usize {
     }
     count
 }
-fn part2(data: &str) -> usize {
-    888
+fn part2(data: &str) -> u64 {
+    
+    let chunks: Vec<_> = data.split("\n\n").filter(|y| !y.is_empty()).collect();
+    let directions = chunks[0].chars().cycle(); // repeating iterator
+    
+    // Build the network
+    let mut node_hashmap: HashMap<String, Node> = HashMap::new();
+    let mut curr_vec:Vec<_> = Vec::new();
+    chunks[1]
+        .split('\n')
+        .filter(|y| !y.is_empty())
+        .map(|x| x.parse::<Node>().unwrap())
+        .map(|x| { node_hashmap.insert(x.from.clone(),x.clone()) })
+        .for_each(drop);
+
+    for k in  node_hashmap.keys() {
+        if k.ends_with('A') { 
+            curr_vec.append(&mut vec![k.to_string()]);
+        };
+    }
+
+    debug!("CURR MAP: {:?}", curr_vec);
+
+    // Walk it
+        
+    let mut count = 0;
+    let mut last_seen = vec![0; curr_vec.len()];
+    let mut periods = vec![0; curr_vec.len()];
+    
+    for d in directions {
+        let mut next_vec:Vec<_> = Vec::new();
+        for (i,current) in curr_vec.into_iter().enumerate() {
+            let next = match d {
+                'L' => &node_hashmap.get(&current).unwrap().left,
+                'R' => &node_hashmap.get(&current).unwrap().right,
+                _   => todo!(),
+            };      
+            next_vec.push( next.to_string());
+            if next.ends_with('Z') { 
+                if periods[i] == 0 && last_seen[i] == 0 {
+                    last_seen[i] = count;
+                } else if periods[i] == 0 {                                 
+                    debug!("[{}] {}",i,count-last_seen[i]);
+                    periods[i] = count-last_seen[i];
+                    last_seen[i] = count;
+                }
+                if periods.clone().into_iter().filter(|x|*x==0).count() == 0 {
+                    break;
+                }
+            }
+        }
+        count +=1;
+
+        // Billions of CPU cycles later we might trigger this...
+        if next_vec.iter().filter(|x| x.ends_with('Z')).count() == next_vec.len() { break; }
+        curr_vec = next_vec;
+
+        if count % 10000000 == 0 {
+        debug!("Count: {}",count);
+        debug!("CURR MAP: {:?}", curr_vec);
+        }
+    }
+    // We recall this from another year - find the periods of each one, and then take the product of their lcm's
+    let mut lcm_result:u64 = periods[0];
+    for p in periods {
+        lcm_result = p.lcm(&lcm_result);
+
+    }  
+    lcm_result
 }
 
 fn main() {
@@ -83,6 +151,6 @@ fn main() {
     assert!(p1 == 22199);
     let p2 = part2(std::fs::read_to_string("input.txt").unwrap().as_str());
     info!("Part2: {}", p2);
-    assert!(p2 == 888);
+    assert!(p2 == 13334102464297);
     info!("Completed in {} us", now.elapsed().as_micros());
 }
