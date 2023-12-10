cf4d32b4f4a61ab3fb1bd00157a08c5d64f2e852
Malcolm Lashley <mlashley@gmail.com>
2023-12-10 14:51:23 +0000

    Day7.2 and Clippy-Lint

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -3,43 +3,55 @@
 // use std::hash::Hash;
 use std::time::Instant;
 use std::env;
-use log::{debug, error, info, log_enabled, warn, Level};
+use log::{debug, error, info, warn};
 use parse_display::Display;
 use std::cmp::Ordering;
 
 fn test() {
 
-    debug!("{:?}",Hand::new("A333A 234"));
-    debug!("{:?}",Hand::new("A323A 2334"));
-    debug!("{:?}",Hand::new("2333A 234"));
-    debug!("{:?}",Hand::new("33333 33"));
-    debug!("{:?}",Hand::new("62344 234"));
-    debug!("{:?}",Hand::new("6234Q 2354"));
-
+    debug!("{:?}",Hand::new("A333A 234",false));
+    debug!("{:?}",Hand::new("A323A 2334",false));
+    debug!("{:?}",Hand::new("2333A 234",false));
+    debug!("{:?}",Hand::new("33333 33",false));
+    debug!("{:?}",Hand::new("62344 234",false));
+    debug!("{:?}",Hand::new("6234Q 2354",false));
 
     // Did we f up our Eq/Ord implementation - let's unit-test
-    let k3 = Hand::new("6333A 234");
-    let p2 = Hand::new("A363A 234");
-    let p22 = Hand::new("3A63A 909");
+    let k3 = Hand::new("6333A 234",false);
+    let p2 = Hand::new("A363A 234",false);
+    let p22 = Hand::new("3A63A 909",false);
     debug_assert!(k3 > p2);
     debug_assert!(k3 == k3);
     debug_assert!(p2 > p22);
-    debug_assert!((p2 == p22) == false);
+    debug_assert!(p2 != p22);
 
+    // What about with Jokers
+    let a1 = Hand::new("J333A 234",false);
+    let a2 = Hand::new("A3633 234",false); 
+    let b1 = Hand::new("J333A 234",true);
+    let b2 = Hand::new("A3633 234",true);  
+    debug_assert!(a1<a2);
+    debug_assert!(b1>b2);
+
+    let b1 = Hand::new("3J33J 234",true);
+    let b2 = Hand::new("J3J33 234",true);  
+    debug_assert!(b1>b2);
 
     debug_assert!(
         part1(
             std::fs::read_to_string("input_sample.txt")
                 .unwrap()
                 .as_str()
+            , false
         ) == 6440
     );
     debug_assert!(
-        part2(
+        part1(
             std::fs::read_to_string("input_sample.txt")
                 .unwrap()
                 .as_str()
-        ) == 888
+                , true
+        ) == 5905
     );
 }
 
@@ -52,10 +64,12 @@ enum HandType {
  FullHouse,
  FourKind,
  FiveKind,
+ Unknown
 }
 
 #[derive(Debug,Ord,PartialOrd,Eq,PartialEq,Clone)]
 enum CardType {
+ Joker,   
  Two,
  Three,
  Four,
@@ -72,7 +86,6 @@ enum CardType {
  Unknown
 }
 
-
 #[derive(Debug,Display,Clone)]
 #[display("format")]
 struct Hand {
@@ -84,17 +97,18 @@ struct Hand {
 
 impl Hand {
     
-    fn new(stri: &str) -> Self {
+    fn new(stri: &str,jokerz: bool) -> Self {
 
         let splitty:Vec<_>= stri.split(' ').collect();
         let v = splitty[1].parse::<u64>().unwrap();
 
-        let mut c:Vec<CardType> = splitty[0].chars().into_iter().map(
+        let mut joker_count = 0;
+        let mut c:Vec<CardType> = splitty[0].chars().map(
             |x| match x {
                 'A' => CardType::Ace,
                 'K' => CardType::King,
                 'Q' => CardType::Queen,
-                'J' => CardType::Jack,
+                'J' => if jokerz {joker_count +=1 ; CardType::Joker} else {CardType::Jack},
                 'T' => CardType::Ten,
                 '9' => CardType::Nine,
                 '8' => CardType::Eight,
@@ -111,28 +125,62 @@ impl Hand {
         let d=c.clone();
 
         c.sort();
-        let mut ht = HandType::HighCard;
+        let mut ht = HandType::Unknown;
         if c[0] == c[4] {
             ht = HandType::FiveKind;
         } else if c[0] == c[3] || c[1] == c[4] { // Annoying bug I missed the 2nd of these... write more test-cases.
-            ht = HandType::FourKind       
+            ht = match joker_count {
+                4 => HandType::FiveKind,
+                1 => HandType::FiveKind,
+                0 => HandType::FourKind,
+                _ => { error!("Their morals, their code—it's a bad joke."); HandType::Unknown}
+            }
+
         } else if (c[0] == c[2] && c[3] == c[4]) || ( c[0] == c[1] && c[2] == c[4]) {
-            ht = HandType::FullHouse;
+            ht = match joker_count {
+                3 => HandType::FiveKind,
+                2 => HandType::FiveKind,
+                0 => HandType::FullHouse,
+                _ => { error!("As you know, madness is like gravity: All it takes is a little push."); HandType::Unknown}
+            }
+
         } else if c[0] == c[2] || c[1] == c[3] || c[2] == c[4] {
-            ht = HandType::ThreeKind;
+            ht = match joker_count {
+                3 => HandType::FourKind,
+                2 => HandType::FiveKind,
+                1 => HandType::FourKind,
+                0 => HandType::ThreeKind,
+                _ => { error!("What merry hell is this?"); HandType::Unknown}
+            }
         } else if 
                     (c[0] == c[1] && (c[2] == c[3] || c[3] == c[4])) 
                     || (c[1] == c[2] && c[3] == c[4])
                  {
-            ht = HandType::TwoPair;
+            ht = match joker_count {
+                2 => HandType::FourKind,
+                1 => HandType::FullHouse,
+                0 => HandType::TwoPair,
+                _ => { error!("The only sensible way to live in this world is without rules."); HandType::Unknown}
+            }
+
         } else if c[0] == c[1] || c[1] == c[2] || c[2] == c[3] || c[3] == c[4] {
-            ht = HandType::OnePair
+            ht = match joker_count {
+                2 => HandType::ThreeKind,
+                1 => HandType::ThreeKind,
+                0 => HandType::OnePair,
+                _ => { error!("A joke a day keeps the gloom away!"); HandType::Unknown}
+            }           
         } else if c[0] != c[1] && c[0] != c[2] && c[0] != c[3] && c[0] != c[4]
         && c[1] != c[2] && c[1] != c[3] && c[1] != c[4] 
         && c[2] != c[3] && c[2] != c[4] 
         && c[3] != c[4] {
         // ^^ Was originally fall thru - but adding for sanity checking when my first solution passed the sample data - but not the answer.
-            ht = HandType::HighCard;
+            ht = match joker_count {
+                1 => HandType::OnePair,
+                0 => HandType::HighCard,
+                _ => { error!("It's not about the money, it's about sending a message. Everything burns!"); HandType::Unknown}
+            }     
+
         } else {
             error!("Hand unparseable... {:?}",splitty);
         }
@@ -147,21 +195,7 @@ impl Hand {
 
 impl PartialOrd for Hand {
     fn partial_cmp(&self, other: &Self) ->Option<std::cmp::Ordering> {
-        if self.hand_type < other.hand_type {
-            return Some(Ordering::Less);
-        }
-        if self.hand_type > other.hand_type {
-            return Some(Ordering::Greater);
-        }
-        for (i,c) in self.cards_v.clone().into_iter().enumerate() {
-            if c > other.cards_v[i] {
-                return Some(Ordering::Greater);
-            } else if c < other.cards_v[i] {
-                return Some(Ordering::Less);
-            }
-            // If equal - consider the next card.
-        }      
-        Some(Ordering::Equal)
+        Some(self.cmp(other)) 
     }
 }
 
@@ -174,18 +208,16 @@ impl Ord for Hand {
             return Ordering::Greater;
         }
         for (i,c) in self.cards_v.clone().into_iter().enumerate() {
-            if c > other.cards_v[i] {
-                return Ordering::Greater;
-            } else if c < other.cards_v[i] {
-                return Ordering::Less;
-            }
-            // If equal - consider the next card.
+            match c.cmp(&other.cards_v[i]) {
+                Ordering::Less => { return Ordering::Less; },
+                Ordering::Greater => { return Ordering::Greater; },
+                Ordering::Equal => {}, // Consider the next card.
+            }            
         }      
         warn!("If this happens our input data has indeterminate answer");
         Ordering::Equal
     }
 }
-
 
 impl PartialEq for Hand {
     fn eq(&self, other: &Self) -> bool {
@@ -193,30 +225,19 @@ impl PartialEq for Hand {
     }
 }
 
-impl Eq for Hand {} // I'm not sure I understand these traits anymore...
+impl Eq for Hand {} // I'm not sure I understand these traits anymore... but the Rust docs say to do this?
 
-
-fn part1(data: &str) -> u64 {
+fn part1(data: &str, b: bool) -> u64 {
     
     let mut hands: Vec<Hand> = data.split('\n')
     .filter(|y| !y.is_empty())
-    .map(|x| Hand::new(x))
+    .map(|x| Hand::new(x,b))
     .collect();
   
     hands.sort();
-
-    // for (i,h) in hands.clone().into_iter().enumerate() {
-    //     debug!("{}  * {} = {}",i,h.value,1+u64::try_from(i).unwrap() *h.value);
-    // }
-    info!("====");
-    hands.clone().into_iter().map(|h|debug!("{:?}",h)).for_each(drop);
-
-    
+    // hands.clone().into_iter().map(|h|debug!("{:?}",h)).for_each(drop);
     hands.into_iter().enumerate().map(|(i,hand)| (1+u64::try_from(i).unwrap()) *hand.value).sum::<u64>()
 
-}
-fn part2(data: &str) -> u64 {
-    888
 }
 
 fn main() {
@@ -227,13 +248,13 @@ fn main() {
     test();
     let now = Instant::now();
     let p1 = part1(
-        std::fs::read_to_string("input.txt").unwrap().as_str()
+        std::fs::read_to_string("input.txt").unwrap().as_str(), false
     );
     info!("Part1: {}", p1);
     assert!(p1 != 251563193 ); // Too high...
     assert!(p1 == 251029473);
-    let p2 = part2(std::fs::read_to_string("input.txt").unwrap().as_str());
+    let p2 = part1(std::fs::read_to_string("input.txt").unwrap().as_str(),true);
     info!("Part2: {}", p2);
-    assert!(p2 == 888);
+    assert!(p2 == 251003917);
     info!("Completed in {} us", now.elapsed().as_micros());
 }
