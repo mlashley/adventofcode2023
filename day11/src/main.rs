8d972334d6988220883b32cb202deee4fbac201f
Malcolm Lashley <mlashley@gmail.com>
2023-12-17 22:42:09 +0000

    Ugly refactor

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -20,95 +20,134 @@ fn test() {
     );
 }
 
-// Function to expand the universe based on the given rules
-fn expand_universe(universe: &mut Vec<Vec<char>>) {
-    let cols = universe[0].len();
+fn print_universe(galaxies: &HashSet<(usize, usize)>) {
 
-    // Identify rows without galaxies and insert new rows above them
-    let mut new_universe = Vec::new();
-    for row in universe.iter() {
-        if !row.contains(&'#') {
-            new_universe.push(vec!['.'; cols]);
+    let x_max = galaxies.iter().map(|g| g.0).max().unwrap() + 1;
+    let y_max = galaxies.iter().map(|g| g.1).max().unwrap() + 1 ;
+    let mut s = String::new();
+    for y in 0..y_max {
+        for x in 0..x_max {
+            if galaxies.contains(&(x,y)) {
+                s.push('#');
+            } else {
+                s.push('.');
+            }
         }
-        new_universe.push(row.clone());
+        s.push('\n');
     }
+    debug!("Universe:\n{}",s);
 
-    debug!("New rows >>>>");
-    for row in &new_universe {
-        debug!("{:?}", row);
-    }
-    debug!("<<<< New rows");
+}
 
-    // Identify columns without galaxies and insert new columns to the left of them
-    let mut expanded_universe = new_universe.clone();
-    let mut added = 0;
-    for col in 0..cols {
-        if !new_universe.iter().any(|row| row[col] == '#') {
-            debug!("New col at {} ({})", col, col + added);
-            for row in expanded_universe.iter_mut() {
-                row.push('.');
-                for c in (col + added..row.len()).rev() {
-                    row[c] = row[c - 1];
+// Function to expand the universe based on the given rules
+fn expand_universe(galaxies:  &mut HashSet<(usize, usize)>,universe: Vec<Vec<char>>, time: usize) {
+    
+    // Identify rows without galaxies and insert new rows above them
+    let mut new_galaxies = galaxies.clone();
+    let mut added = 0 ;
+    
+    for (y,row) in universe.iter().enumerate() {
+        if !row.contains(&'#') {
+            debug!("New row at {}",y);
+            for galaxy in new_galaxies.clone().iter() {
+                if galaxy.1 > y+added {
+                    new_galaxies.remove(galaxy);
+                    new_galaxies.insert((galaxy.0,galaxy.1+time));
                 }
-                row[col + added] = '.';
             }
             added += 1;
-
-            debug!("New cols >>>>");
-            for row in &expanded_universe {
-                debug!("{:?}", row);
-            }
-            debug!("<<<< New cols");
         }
+        
     }
 
-    *universe = expanded_universe;
+    let mut v = Vec::from_iter(new_galaxies.clone()); v.sort();
+    debug!("new_galaxies:   {:?}",v);
+
+    // Identify columns without galaxies and insert new columns to the left of them
+    
+    let cols = universe[0].len();
+    let mut added = 0;
+    for col in 0..cols {
+        if !universe.iter().any(|row| row[col] == '#') {
+            debug!("New col at {}", col);
+            for galaxy in new_galaxies.clone().iter() {
+                if galaxy.0 > col+added {
+                    new_galaxies.remove(galaxy);
+                    new_galaxies.insert((galaxy.0+time,galaxy.1));
+                }
+            }
+            added += 1;
+            let mut v = Vec::from_iter(new_galaxies.clone()); v.sort();
+            debug!("newer_galaxies: {:?}",v);
+        }
+    }
+    debug!("newer_galaxies: {:?}",new_galaxies);
+    print_universe(&new_galaxies);
+
+    *galaxies = new_galaxies;
 }
 // Function to find the shortest path length between two galaxies
-fn shortest_path_length(start: (usize, usize), end: (usize, usize)) -> i64 {
+fn shortest_path_length(start: &(usize, usize), end: &(usize, usize)) -> i64 {
     (end.1 as i64 - start.1 as i64).abs() + (end.0 as i64 - start.0 as i64).abs()
 }
 
 // Function to calculate the sum of shortest path lengths between all pairs of galaxies
-fn sum_of_shortest_paths(universe: &Vec<Vec<char>>) -> i64 {
+fn sum_of_shortest_paths(galaxies: HashSet<(usize, usize)>) -> i64 {
     let mut total_length = 0;
     let mut seen = HashSet::new();
 
-    for i in 0..universe.len() {
-        for j in 0..universe[0].len() {
-            if universe[i][j] == '#' {
-                for x in 0..universe.len() {
-                    for y in 0..universe[0].len() {
-                        if universe[x][y] == '#'
-                            && !seen.contains(&(i, j, x, y)) & !seen.contains(&(x, y, i, j))
-                        {
-                            let length = shortest_path_length((i, j), (x, y));
-                            seen.insert((i, j, x, y));
-                            debug!("{},{} => {},{} tot: {}", i, j, x, y, total_length);
-                            total_length += length;
-                        }
-                    }
-                }
+    for galaxy_from in &galaxies {
+        for galaxy_to in &galaxies {
+            if galaxy_from != galaxy_to 
+            && !seen.contains(&(galaxy_from,galaxy_to)) 
+            && !seen.contains(&(galaxy_to,galaxy_from)) {
+                seen.insert((galaxy_from,galaxy_to));
+            
+                let mut length = shortest_path_length(galaxy_from,galaxy_to);
+                debug!("{},{} => {},{} len: {} tot: {}",galaxy_from.0,galaxy_from.1, galaxy_to.0, galaxy_to.1,length,total_length);
+                total_length += length;
             }
         }
     }
-
     total_length
 }
 
 fn part1(data: &str) -> i64 {
-    let mut universe: Vec<_> = data
-        .split('\n')
-        .filter(|y| !y.is_empty())
-        .map(|x| x.chars().collect::<Vec<char>>())
-        .collect();
 
-    expand_universe(&mut universe);
 
-    let result = sum_of_shortest_paths(&universe);
+    let universe: Vec<_> = data
+    .split('\n')
+    .filter(|y| !y.is_empty())
+    .map(|x| x.chars().collect::<Vec<char>>())
+    .collect();
+
+    let mut galaxies = HashSet::new();
+    data
+    .split('\n')
+    .filter(|y| !y.is_empty())
+    .enumerate()
+    .map(|(y, x)| 
+        for (xx,c) in x.chars().enumerate() {
+            if c == '#' { 
+                galaxies.insert((xx,y));
+            } 
+        }
+    )
+    .for_each(drop);
+
+    debug!("We have {} galaxies",galaxies.len());
+
+    debug!("{:?}",galaxies);
+
+    expand_universe(&mut galaxies,universe,1);
+
+    debug!("{:?}",galaxies);
+
+    let result = sum_of_shortest_paths(galaxies);
 
     debug!("Sum of shortest path lengths: {}", result);
     result
+    
 }
 fn part2(data: &str) -> i64 {
     888
